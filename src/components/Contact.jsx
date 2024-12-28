5a5705c15e30061855f41b6e758d0bec8fc87be8
Rohan Pantam <84071174+pantamrohan@users.noreply.github.com>
2024-12-28 22:08:24 +0530

    Update Contact.jsx
    right again

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -19,7 +19,7 @@ const Contact = () => {
                     <ImageClipBox clipClass="contact-clip-path-2 translate-y-60 lg:translate-y-60"
                     src="img/contact-2.webp"/>
                 </div>
-                <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-5 lg:top-20 lg:w-80">
+                <div className="absolute -top-40 left-20 w-60 sm:top-1/2 md:left-auto md:right-2 lg:top-20 lg:w-80">
                     <ImageClipBox 
                     src="img/swordman-partial.webp"
                     clipClass="absolute md:scale-115"/>
