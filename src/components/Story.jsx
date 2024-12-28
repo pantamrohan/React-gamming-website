e2382beb6ed1ec82c2db5598abbf1c5a94f982fe
Rohan Pantam <84071174+pantamrohan@users.noreply.github.com>
2024-12-28 22:00:52 +0530

    Update Story.jsx
    margin top to 10

--- src/components/Story.jsx
+++ src/components/Story.jsx
@@ -68,7 +68,7 @@ const Story = () => {
 
             <div className="-mt-80 sm:-mt-20 flex w-full justify-center md:-mt-64 md: me-44 md:justify-end ">
                 <div className="flex ml-40 h-full w-fit flex-col items-center md:items-start">
-                    <p className="mt-3  max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+                    <p className="mt-10  max-w-sm text-center font-circular-web text-violet-50 md:text-start">
                     Where realms converge, lies Zentry and the boundless pillar. Discover its secrets and shape your fate amidst infinite opportunities.
                     </p>
                     <Button id="realm-button" title="discover prologue" containerClass="mt-5"/>
